a3a9e3d56d363e3049c415c33f09cdb2783c0a1e
Liam May <liam_may@knights.ucf.edu>
2020-03-23 10:21:25 -0400

    Update SSHW3.c

--- SSHW3.c
+++ SSHW3.c
@@ -53,7 +53,7 @@ typedef struct
 
 
 char* trim(char *str, char *trimmed);
-int parse(char *code, lexeme list[], FILE *fplex);
+int parse(char *code, lexeme list[], FILE *fplex, symbol symbol_table[]);
 bool isReserved(char *str);
 token_type reserved(char *str);
 lexeme *createLexeme(token_type t, char *str);
@@ -104,10 +104,10 @@ char* trim(char *str, char *trimmed)
 // Parses words from the code to be evaluated, adds them to the lexeme array,
 // and returns the number of lexemes that were added to the array or 0 if there
 // was an error
-int parse(char *code, lexeme list[], FILE *fplex)
+int parse(char *code, lexeme list[], FILE *fplex, symbol symbol_table[])
 {
   lexeme *lexptr;
-  int lp = 0, rp, length, i, listIndex = 0;
+  int lp = 0, rp, length, i, listIndex = 0, symIndex = 0;
   char buffer[MAX_CODE_LENGTH];
   token_type t;
 
@@ -642,6 +642,11 @@ void getToken(token current)
     }
     strcpy(current.value, buffer);
   }
+  else
+  {
+    current.type[0] = '\0';
+    current.value[0] = '\0';
+  }
 }
 
 void findError(int errorNum)
@@ -649,117 +654,120 @@ void findError(int errorNum)
     switch( errorNum )
     {
 
-    case 1:
-      printf("Use = instead of := \n");
-      break;
+      case 1:
+        printf("Use = instead of := \n");
+        break;
 
-    case 2:
-      printf("= must be followed by a number \n");
-      break;
+      case 2:
+        printf("= must be followed by a number \n");
+        break;
 
-    case 3:
-      printf("Identifier must be followed by = \n");
-      break;
+      case 3:
+        printf("Identifier must be followed by = \n");
+        break;
 
-    case 4:
-      printf("const, int, procedure must be followed by identifier\n");
-      break;
+      case 4:
+        printf("const, int, procedure must be followed by identifier\n");
+        break;
 
-    case 5:
-      printf("Semicolon or comma missing\n");
-      break;
+      case 5:
+        printf("Semicolon or comma missing\n");
+        break;
 
-    case 6:
-      printf("Incorrect symbol after procedure declaration\n");
-      break;
+      case 6:
+        printf("Incorrect symbol after procedure declaration\n");
+        break;
 
-    case 7:
-      printf("Statement expected\n");
-      break;
+      case 7:
+        printf("Statement expected\n");
+        break;
 
-    case 8:
-      printf("Incorrect symbol after statement part in block\n");
-      break;
+      case 8:
+        printf("Incorrect symbol after statement part in block\n");
+        break;
 
-    case 9:
-      printf("Period expected\n");
-      break;
+      case 9:
+        printf("Period expected\n");
+        break;
 
-    case 10:
-      printf("Semicolon between statements missing\n");
-      break;
+      case 10:
+        printf("Semicolon between statements missing\n");
+        break;
 
-    case 11:
-      printf("Undeclared identifier \n");
-      break;
+      case 11:
+        printf("Undeclared identifier \n");
+        break;
 
-    case 12:
-      printf("Assignment to constant or procedure is not allowed\n");
-      break;
+      case 12:
+        printf("Assignment to constant or procedure is not allowed\n");
+        break;
 
-    case 13:
-      printf("Assignment operator expected\n");
-      break;
+      case 13:
+        printf("Assignment operator expected\n");
+        break;
 
-    case 14:
-      printf("Call must be followed by an identifier\n");
-      break;
+      case 14:
+        printf("Call must be followed by an identifier\n");
+        break;
 
-    case 15:
-      printf("Call of a constant or variable is meaningless\n");
-      break;
+      case 15:
+        printf("Call of a constant or variable is meaningless\n");
+        break;
 
-    case 16:
-      printf("Then expected\n");
-      break;
+      case 16:
+        printf("Then expected\n");
+        break;
 
-    case 17:
-      printf("Semicolon or } expected \n");
-      break;
+      case 17:
+        printf("Semicolon or } expected \n");
+        break;
 
-    case 18:
-      printf("Do expected\n");
-      break;
+      case 18:
+        printf("Do expected\n");
+        break;
 
-    case 19:
-      printf("Incorrect symbol following statement\n");
-      break;
+      case 19:
+        printf("Incorrect symbol following statement\n");
+        break;
 
-    case 20:
-      printf("Relational operator expected\n");
-      break;
+      case 20:
+        printf("Relational operator expected\n");
+        break;
 
-    case 21:
-      printf("Expression must not contain a procedure identifier\n");
-      break;
+      case 21:
+        printf("Expression must not contain a procedure identifier\n");
+        break;
 
-    case 22:
-      printf("Right parenthesis missing\n");
-      break;
+      case 22:
+        printf("Right parenthesis missing\n");
+        break;
 
-    case 23:
-      printf("The preceding factor cannot begin with this symbol\n");
-      break;
+      case 23:
+        printf("The preceding factor cannot begin with this symbol\n");
+        break;
 
-    case 24:
-      printf("An expression cannot begin with this symbol\n");
-      break;
+      case 24:
+        printf("An expression cannot begin with this symbol\n");
+        break;
 
-    case 25:
-      printf("This number is too large\n");
-      break;
+      case 25:
+        printf("This number is too large\n");
+        break;
 
       default:
       printf("Invalid instruction");
     }
 }
 
-void insertSymbols(int counter, symbol symbol_table[], int kind, char name[], int val, int level, int addr)
+// Stores passed values in symbol table
+// For constants (28), you must store kind, name and value.
+// For variables (29), you must store kind, name, L and M.
+void insertSymbol(int counter, symbol symbol_table[], int kind, char name[], int val, int level, int addr)
 {
-  //filling in the symbol tables with current data
-  //note: counter needs to be set to zero in main
+  // Filling in the symbol tables with current data
+  // Note: counter needs to be set to zero in main
   symbol_table[counter].kind = kind;
-  //capture name string and insert it into symbol table as a string
+  // Captures name string and insert it into symbol table as a string
   strcpy(symbol_table[counter].name, name);
   symbol_table[counter].val = val;
   symbol_table[counter].level = level;
